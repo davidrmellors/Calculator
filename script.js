3ab7f77382ae1601e7eabfbc12d8851e06b66c35
David Mellors <davidroymellors@gmail.com>
2024-01-03 15:07:39 +0200

    Add operation functions and start UI

---
+++ script.js
@@ -0,0 +1,41 @@
+let firstNumber = 0;
+let operator = "";
+let secondNumber = 0;
+
+
+
+
+function operate(num1, operator, num2) {
+    switch(operator){
+        case "+":
+            add(num1, num2);
+        break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+        default:
+            break;
+    }
+}
+
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
